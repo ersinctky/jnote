1a498393f7c37ef3e6debfa46bf486829d0ad1ff
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-04-05 23:36:22 +0300

    Bundling Redux

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,41 @@
-export const test = 'asdasd';
+import produce from 'immer';
+import { Reducer } from 'redux';
+import { BundleActionType } from '../actionTypes';
+import { Action } from '../actions';
+
+interface BundlesState {
+  [key: string]:
+    | {
+        loading: boolean;
+        code: string;
+        error: string;
+      }
+    | undefined;
+}
+
+const initialState: BundlesState = {};
+
+const reducer: Reducer<BundlesState, Action> = produce((state = initialState, action) => {
+  switch (action.type) {
+    case BundleActionType.BUNDLE_START:
+      state[action.payload.cellId] = {
+        loading: true,
+        code: '',
+        error: '',
+      };
+      return;
+
+    case BundleActionType.BUNDLE_COMPLETE:
+      state[action.payload.cellId] = {
+        loading: false,
+        code: action.payload.bundle.code,
+        error: action.payload.bundle.error,
+      };
+      return;
+
+    default:
+      return state;
+  }
+});
+
+export default reducer;
